1b86a38aaebebdc863b0368f7e4c5ba20c68cef1
Khanh <khanhnguyenlucas@gmail.com>
2024-06-18 06:22:53 +0700

    First Commit

---
+++ app/src/main/java/com/example/notepad/NotesDatabaseHelper.kt
@@ -0,0 +1,58 @@
+package com.example.notepad
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "notepad.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "allnotes"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_TITLE = "title"
+        private const val COLUMN_CONTENT = "content"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createQuery = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_TITLE TEXT, $COLUMN_CONTENT TEXT)"
+        db?.execSQL(createQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        val dropQuery = "DROP TABLE IF EXISTS $TABLE_NAME"
+        db?.execSQL(dropQuery)
+        onCreate(db)
+    }
+
+    fun insertNote(note: Note){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE, note.title)
+            put(COLUMN_CONTENT, note.content)
+        }
+        db.insert(TABLE_NAME, null, values)
+        db.close()
+    }
+
+    fun getAllNote(): List<Note>{
+        val noteList = mutableListOf<Note>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query,null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val note = Note(id,title,content)
+            noteList.add(note)
+        }
+        cursor.close()
+        db.close()
+        return noteList
+    }
+}
